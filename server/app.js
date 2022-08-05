0b374c9198c9cc116d465e0565aa065210b9a21c
Feifei Liang <mayliang021@gmail.com>
2022-08-05 14:18:09 -0500

    update loader token

--- server/app.js
+++ server/app.js
@@ -10,6 +10,8 @@ const app = express();
 const productsRoutes = require('./routes/products');
 const cartRoutes = require('./routes/cart');
 
+app.use(express.json())
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
@@ -18,11 +20,10 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json())
 
-app.get('/loaderio-4a9893ae9f2a54c3ce7dcb59361ab52c.txt', async(req, res) => {
+app.get('/loaderio-7bea325eba033e162ff1df38a489c505.txt', async(req, res) => {
   try {
-    res.status(200).sendFile(path.join(__dirname, 'loaderio-4a9893ae9f2a54c3ce7dcb59361ab52c.txt'))
+    res.status(200).send('loaderio-7bea325eba033e162ff1df38a489c505');
   } catch (e) {
     console.log('errrrrrr here', e)
   }
